c115c0641c95565a0bbceeb28250b066b4276d21
Will Harris <mosinu@gmail.com>
2020-03-07 14:16:40 -0500

    fixed shad512

--- hash/sha512.go
+++ hash/sha512.go
@@ -1,6 +1,24 @@
-//placeholder
 package hash
 
-func Sha512sum(filePath string) (string, error) {
+import (
+	"crypto/sha512"
+	"encoding/hex"
+	"io"
+	"os"
+)
 
+func Sha512sum(filePath string) (string, error) {
+	var returnSHA512String string
+	file, err := os.Open(filePath)
+	if err != nil {
+		return returnSHA512String, err
+	}
+	defer file.Close()
+	hash := sha512.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnSHA512String, err
+	}
+	hashInBytes := hash.Sum(nil)[:64]
+	returnSHA512String = hex.EncodeToString(hashInBytes)
+	return returnSHA512String, nil
 }
